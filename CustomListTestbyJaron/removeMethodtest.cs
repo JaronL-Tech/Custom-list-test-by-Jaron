11967d49abb4df79cd2f86b4fb2d93f2b7b417a8
Jaron <Jaronleach98@gmail.com>
2023-08-08 01:44:48 -0400

    Finished creating the a removed method test.

--- CustomListTestbyJaron/removeMethodtest.cs
+++ CustomListTestbyJaron/removeMethodtest.cs
@@ -32,11 +32,30 @@ namespace CustomListTestbyJaron
             //arrange
             CustomList<int> mylist = new CustomList<int>();
             //Act
-            mylist.Remove(item);
+            mylist.Remove(1);
             //Assert
             Assert.AreEqual(4, mylist.Items);
         }
         public void removemethod_itemshiftsbackwards_filltheindexwithremoveditem()
+        {
             //arrange
+            CustomList<int> mylist = new CustomList<int>();
+            mylist.Add(2);
+            mylist.Add(items);
+            //act
+            mylist.Remove(item);
+            //assert
+            Assert.AreEqual(1,mylist.Items);
+        }
+        public void removingmetod_removeanitemthatexist_multipleinstances()
+        {
+            //arrange
+            CustomList<int> mylist = new CustomList<int>();
+            //act
+            mylist.Remove(2);
+            //assert
+            Assert.AreEqual(2, mylist.Count);
+        }
     }
+            
 }
